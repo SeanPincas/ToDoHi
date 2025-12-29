8f7c0e4a7f2f4d7510361aa2ad3ed9647b2be445
Sean Pincas <seanpincas28@gmail.com>
2025-12-30 01:06:44 +0800

    more changes

--- frontend/src/components/todo/SortableTaskItem.tsx
+++ frontend/src/components/todo/SortableTaskItem.tsx
@@ -64,8 +64,13 @@ export const SortableTaskItem = ({
             style={containerStyle}
             className={`task-item-container ${isDragging ? "dragging" : ""}`}
             onClick={() => {
-                // Prevent opening ViewModal while in rearrange or delete mode
-                if (!isRearrangeMode && !isDeleteMode) {
+                // Prevent opening ViewModal while in delete mode instead its for delete selection
+                if (isDeleteMode) {
+                    toggleDeleteSelection(task._id);
+                    return;
+                }
+                // Prevent opening ViewModal while in rearrange
+                if (!isDeleteMode) {
                     onOpenView();
                 }
             }}
